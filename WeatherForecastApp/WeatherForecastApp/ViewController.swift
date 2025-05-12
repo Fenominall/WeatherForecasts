4a09ffd515cf4fe681daea073c1603d927e18979
Vladislav Todorov <it.vtodorov@gmail.com>
2025-05-12 18:37:17 +0300

    Did create new project for the WeatherForecastApp

---
+++ WeatherForecastApp/WeatherForecastApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherForecastApp
+//
+//  Created by Fenominall on 5/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
