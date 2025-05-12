3174332b2fba020bccf71718b989a0ab9d939ae4
Vladislav Todorov <it.vtodorov@gmail.com>
2025-05-12 19:04:41 +0300

    add: Did add location model that will containt the info about the user selected location to fetch necessary API data

---
+++ WeatherForecastApp/Domain/WFCoreDomain/Sources/WFCoreDomain/Models/Location.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  WFCoreDomain
+//
+//  Created by Fenominall on 5/12/25.
+//
+
+import Foundation
+
+struct Location {
+    let name: String
+    let country: String
+    let latitude: Double
+    let longitude: Double
+    let localTime: String
+}
